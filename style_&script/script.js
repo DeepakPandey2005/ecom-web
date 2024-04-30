8a33534f7888125a16a651b672c3697ae3fbe78c
Deepak <deepak481481481@gmail.com>
2024-04-30 14:55:58 +0530

    all the files are arranged specificly

---
+++ style_&script/script.js
@@ -0,0 +1,3 @@
+let username=prompt("Hello dear customer please enter your name :");
+
+alert("Dear user "+username +" , welcome to our  ecom web this is blue print of the incoming web in future so you can test this web and let me know your feedback later");
